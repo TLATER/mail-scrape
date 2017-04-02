e0c45e02e9126e528f230171ddfcf0af5fe19388
Tristan Maat <tm@tlater.net>
2017-04-03 00:58:59 +0100

    Add scraping for unstructured data

--- scrape.js
+++ scrape.js
@@ -11,6 +11,61 @@ const knwl = require("knwl.js");
 const async = require("async");
 
 /**
+ * Test if the given object is a generic object.
+ *
+ * See http://stackoverflow.com/a/37164538.
+ *
+ * @param {Object} item - The object to test
+ * @returns {boolean} Whether the object is a generic object.
+ */
+function is_object(item) {
+    return (item && typeof item === "object"
+            && !Array.isArray(item) && item !== null);
+}
+
+/**
+ * Merge the given objects.
+ *
+ * See http://stackoverflow.com/a/37164538. Modified to also deeply
+ * merge arrays.
+ *
+ * @param {Object} a - The first object.
+ * @param {Object} b - The second object.
+ * @returns {Object} The resulting object.
+ */
+function merge_deep(a, b) {
+    let output = Object.assign({}, a);
+
+    if (is_object(a) && is_object(b)) {
+        Object.keys(b).forEach(key => {
+            if (is_object(b[key])) {
+                if (!(key in a))
+                    Object.assign(output, { [key]: b[key] });
+                else
+                    output[key] = merge_deep(a[key], b[key]);
+
+            } else if (Array.isArray(b[key])) {
+                if (Array.isArray(a[key])) {
+                    output[key] = a[key];
+
+                    for (let element of b[key])
+                        if (!a[key].includes(element))
+                            output[key].push(element);
+                } else {
+                    output[key] = b[key];
+                    output[key].push(a[key]);
+                }
+
+            } else {
+                Object.assign(output, { [key]: b[key] });
+            }
+        });
+    }
+
+    return output;
+}
+
+/**
  * Extract information made available through json-ld in standard
  * script[type="application/ld+json"] tags, if available.
  *
@@ -33,10 +88,67 @@ function extract_json_ld ($, callback) {
         else
             callback(null, json);
     }, (error, results) => {
-        callback(error, results.reduce((acc, e) => {
-            return $.extend(acc, e);
-        }));
+        if (results.length < 1)
+            callback(error, null);
+        else
+            callback(error, results.reduce((acc, e) => {
+                return cheerio.extend(acc, e);
+            }));
     });
+}
+
+function extract_unstructured ($, callback) {
+    let parser = new knwl();
+
+    parser.init($.html());
+
+    let knwl_parsers = [
+        "phones",
+        "emails",
+        "places",
+        "links"
+    ];
+
+    let test = knwl_parsers.map(e => parser.get(e));
+    test = test.reduce((acc, result) => {
+        // Check which type the result is and add it to the correct
+        // schema.org array.
+        for (let e of result) {
+            // Pointless switch for clarity
+            switch(true) {
+            case e["address"] !== undefined:
+                if (!acc["email"])
+                    acc["email"] = [e["address"]];
+                else
+                    acc["email"].push(e["address"]);
+                break;
+
+            case e["place"] !== undefined:
+                if (!acc["place"])
+                    acc["place"] = [{"@type": "Place", name: e["place"]}];
+                else
+                    acc["place"].push({"@type": "Place", name: e["place"]});
+                break;
+
+            case e["phone"] !== undefined:
+                if (!acc["telephone"])
+                    acc["telephone"] = [e["phone"]];
+                else
+                    acc["telephone"].push(e["phone"]);
+                break;
+
+            case e["link"] !== undefined:
+                if (!acc["sameAs"])
+                    acc["sameAs"] = [e["link"]];
+                else
+                    acc["sameAs"].push(e["link"]);
+            }
+        }
+
+        return acc;
+    }, {"@context": "http://schema.org"});
+
+    callback(null, test);
 }
 
 /**
@@ -53,6 +165,8 @@ function get_data (html, callback) {
 module.exports = {
     get_data: get_data,
     _private: {
-        extract_json_ld: extract_json_ld
+        extract_json_ld: extract_json_ld,
+        extract_unstructured: extract_unstructured,
+        merge_deep: merge_deep
     }
 };
--- test/test_scrape.js
+++ test/test_scrape.js
@@ -6,13 +6,134 @@ const fs = require("fs");
 
 const expect = chai.expect;
 
+describe("scrape.merge_deep", () => {
+    it("should combine two objects with nested arrays", () => {
+        let a = {
+            emails: [
+                "a@a.com",
+                "b@b.net"
+            ],
+            peter: {
+                emails: [
+                    "test@test.net"
+                ]
+            }
+        };
+        let b = {
+            emails: [
+                "a@a.com",
+                "c@c.net"
+            ],
+            name: "test"
+        };
+
+        let result = {
+            name: "test",
+            emails: [
+                "a@a.com",
+                "b@b.net",
+                "c@c.net"
+            ],
+            peter: {
+                emails: [
+                    "test@test.net"
+                ]
+            }
+
+        };
+
+        let test = scrape._private.merge_deep(a, b);
+        expect(test).to.deep.equal(result);
+    });
+});
+
+
+/**
+ * Test module:scrape.extract_unstructured
+ */
+describe("scrape.extract_unstructured", () => {
+    it.skip("should extract phone numbers and put them in the correct tag", done => {
+        let numbers = {
+            html: () => "+44 161 414 1080"
+        };
+        let expected = {
+            "@context": "http://schema.org",
+            telephone : [
+                "+44 (161) 414-1080"
+            ]
+        };
+
+        scrape._private.extract_unstructured(numbers, (err, res) => {
+            expect(res).to.deep.equal(expected);
+            done();
+        });
+    });
+
+    it("should extract urls and put them in the correct tag", done => {
+        let urls = {
+            html: () => "http://www.facebook.com"
+        };
+        let expected = {
+            "@context": "http://schema.org",
+            sameAs: ["http://www.facebook.com"]
+        };
+
+        scrape._private.extract_unstructured(urls, (err, res) => {
+            expect(res).to.deep.equal(expected);
+            done();
+        });
+    });
+
+    it("should extract emails and put them in the correct tag", done => {
+        let emails = {
+            html: () => "tim@candii.com\ntw@tlater.net"
+        };
+        let expected = {
+            "@context": "http://schema.org",
+            email: [
+                "tim@candii.com",
+                "tw@tlater.net"
+            ]
+        };
+
+        scrape._private.extract_unstructured(emails, (err, res) => {
+            expect(res).to.deep.equal(expected);
+            done();
+        });
+    });
+
+    it("should extract places and put them in the correct tag", done => {
+        let places = {
+            html: () => "at New York\n at Madrid"
+        };
+        let expected = {
+            "@context": "http://schema.org",
+            place: [
+                {
+                    "@type": "Place",
+                    name: "New York"
+                },
+                {
+                    "@type": "Place",
+                    name: "Madrid"
+                }
+            ]
+        };
+
+        scrape._private.extract_unstructured(places, (err, res) => {
+            expect(res).to.deep.equal(expected);
+            done();
+        });
+    });
+});
+
 /**
  * Test module:scrape.extract_json_ld
  *
  */
 describe("scrape.extract_json_ld", () => {
     /** Ensure that json_ld extraction works for canddi.com */
-    it("should extract the correct data from canddi.com", (done) => {
+    it("should extract the correct data from canddi.com", done => {
         fs.readFile("test/test_data/canddi.html", "utf8", (error, html) => {
             if (error) {
                 done(error);
@@ -31,35 +152,31 @@ describe("scrape.extract_json_ld", () => {
                     logo: "https://www.canddi.com/static/img/logo2x.png",
                     foundingDate: "2009",
                     address:
-                    { "@type": "PostalAddress",
-                      streetAddress: "47 Newton Street",
-                      addressLocality: "Manchester",
-                      addressRegion: "Manchester",
-                      postalCode: "M1 1FT",
-                      addressCountry: "GB" },
+                    {
+                        "@type": "PostalAddress",
+                        streetAddress: "47 Newton Street",
+                        addressLocality: "Manchester",
+                        addressRegion: "Manchester",
+                        postalCode: "M1 1FT",
+                        addressCountry: "GB"
+                    },
                     contactPoint:
-                    { "@type": "ContactPoint",
-                      contactType: "customer support",
+                    {
+                        "@type": "ContactPoint",
+                        contactType: "customer support",
                         telephone: "+44 161 1080",
-                      email: "hello@canddi.com" },
+                        email: "hello@canddi.com"
+                    },
                     sameAs:
-                      [ "https://www.facebook.com/thisiscanddi",
+                    [
+                        "https://www.facebook.com/thisiscanddi",
                         "https://www.linkedin.com/company/1079436",
-                        "https://twitter.com/canddi/" ]
+                        "https://twitter.com/canddi/"
+                    ]
                 });
 
                 done();
             });
         });
-    });
-
-    it("should extract the correct data from a non-compact json_ld", (done) => {
-        var doc = {
-            "http://schema.org/name": "Manu Sporny",
-            "http://schema.org/url": {"@id": "http://manu.sporny.org/"},
-            "http://schema.org/image": {"@id": "http://manu.sporny.org/images/manu.png"}
-        };
-
-
     });
 });
